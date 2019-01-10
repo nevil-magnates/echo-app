2f7fc8b6bf1f452c32e5dcd0ec7b336b9fcf5f42
Nevil Patel <nevil.gambhava@magnates.mobi>
2019-01-10 15:04:33 +0530

    Chnage in Code

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ restService.get("/", function(req, res) {
   });
 });
 
-restService.get("/test", function(req, res) {
+restService.post("/test", function(req, res) {
     var a = request('https://vpic.nhtsa.dot.gov/api/vehicles/getallmanufacturers?format=json', function (error, response, body) {
           console.log('error:', error); // Print the error if one occurred and handle it
           console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
